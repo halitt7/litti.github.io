eb4a33744fead51579b4f172c47fe815f57d674a
halitt7 <halitaliti8@gmail.com>
2024-09-07 17:18:38 -0700

    Update script(1).js
    https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3YyMTNkNnUxdXVtajR1cjZ2bGlleGd5ajR3dXFtd3NqczY3eGU0ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/auGFCmg6rM0eI/giphy.gif

--- script(1).js
+++ script(1).js
@@ -6,7 +6,7 @@ const gif = document.querySelector(".gif");
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
   question.innerHTML = " ta punova PLAS (BAU) 😘 ";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3YyMTNkNnUxdXVtajR1cjZ2bGlleGd5ajR3dXFtd3NqczY3eGU0ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/auGFCmg6rM0eI/giphy.gif";
 });
 
 // Make the No button move randomly on hover
