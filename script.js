c41c734d62ed746c020d1d4f844e3fc4ab38b74f
halitt7 <halitaliti8@gmail.com>
2024-09-08 13:18:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
+const gif = document.querySelector(".https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3YyMTNkNnUxdXVtajR1cjZ2bGlleGd5ajR3dXFtd3NqczY3eGU0ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/auGFCmg6rM0eI/giphy.gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "You got Rickrolled 😘 (FOLLOW ME @kristel_tech or 7 years bad luck)";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3YyMTNkNnUxdXVtajR1cjZ2bGlleGd5ajR3dXFtd3NqczY3eGU0ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/auGFCmg6rM0eI/giphy.gif";
 });
 
 // Make the No button move randomly on hover
